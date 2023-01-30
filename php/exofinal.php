faf75fbce0fea3390bd2db4afed812ac649e1504
NGANAMODEIJunior <nganajunior41@gmail.com>
2023-01-30 16:24:06 +0100

    sdtfgb

---
+++ php/exofinal.php
@@ -0,0 +1,21 @@
+<?php
+function loginForm($password)
+{
+    if($_SERVER['REQUEST_METHOD'] =='POST'){
+       if(isset($_POST['password']) && $_POST['password'] == $password){ return 'ok';
+    }
+    else
+    {
+        echo 'Mot de passe incorrect...';
+    }
+}
+
+echo '<form method="post">Mot de passe: <input type="password" name="password" /> 
+<input type= "submit" value="Se connecter"/></form>';
+}
+$password ='root';
+if(loginForm($password)=='ok'){
+    echo 'mot de passe correct.. <a
+    href="https://wwww.youtube.com/watch?v=h-guAoE19E8&ab_channel=MonsieurRapidecho"> Lien secret!</a>';
+
+}?>
